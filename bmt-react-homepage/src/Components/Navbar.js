df5fdcef6db955d2948a50bf0388f52b04f9ecb2
jeffberlin <jeff@jbrally.com>
2020-12-16 17:55:59 -0500

    Building React

---
+++ bmt-react-homepage/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Navigation() {
+  return (
+    <Navbar expand="lg">
+      <Navbar.Brand href="#home">
+        <img src="../public/images/bmt-logo-2.png" alt="" />
+      </Navbar.Brand>
+    </Navbar>
+  )
+}
+
+export default Navigation;
