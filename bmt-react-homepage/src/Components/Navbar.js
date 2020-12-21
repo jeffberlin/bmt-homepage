5a970c8fd512f185a7f1768e00783d1afee213a7
jeffberlin <jeff@jbrally.com>
2020-12-21 16:03:17 -0500

    Building react

--- bmt-react-homepage/src/Components/Navbar.js
+++ bmt-react-homepage/src/Components/Navbar.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
 import logo from '../images/bmt-logo-2.png';
 
 function Navigation() {
   return (
     <Container>
-      <Navbar>
+      <Navbar defaultExpanded>
         <Navbar.Brand href="#home">
-          <img src={logo} alt="BMT Micro logo" width={120} />
+          <Image src={logo} alt="BMT Micro logo" width={120} />
         </Navbar.Brand>
         <Nav className="justify-content-center">
           <Nav.Item>
