95837dab9b079db365df547e476706e1c9851a2f
jeffberlin <jeff@jbrally.com>
2021-04-12 16:00:39 -0400

    Building new nav layout

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,11 @@
 // opens dropdown menu on hover
-$('div.navbar-nav li.dropdown').hover(function() {
+$('ul.navbar-nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').toggleClass('show');
 });
 
 // handles scroll event on down arrow click
 $(".down-arrow").click(function() {
   $('html, body').animate({
-    scrollTop: $('#section').offset().top
+    scrollTop: $('#section').offset().top - 68
   }, 'slow')
 });
