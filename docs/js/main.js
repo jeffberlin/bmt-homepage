c2c00062a9e98bb55ac65121bc1a2add99434120
jeffberlin <jeff@jbrally.com>
2021-06-17 10:06:16 -0400

    Building

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,10 +2,3 @@
 $('ul.navbar-nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').toggleClass('show');
 });
-
-// handles scroll event on down arrow click
-$(".down-arrow").click(function() {
-  $('html, body').animate({
-    scrollTop: $('#section').offset().top - 68
-  }, 'slow')
-});
