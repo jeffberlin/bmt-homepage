5c019831820cda982816878a0ad77fe2a1a2abed
jeffberlin <jeff@jbrally.com>
2021-06-30 16:01:12 -0400

    Building react pages

--- bmt-homepage-react/src/Components/News.js
+++ bmt-homepage-react/src/Components/News.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Modal, Button } from 'react-bootstrap';
+import POPolicies from './News/PurchaseOrderPolicies';
 import Footer from './Footer';
 
+
+
 function News() {
+	const [modalShow, setModalShow] = React.useState(false);
+
 	return (
 		<>
-			{/*<div className="wrapper">
-				<h1>News</h1>
-			</div>*/}
 			<Navigation />
-				<Container fluid className="news">
-					<Row>
-						<Col className="new-year-header">
-							<h1>A New Year’s message from BMT Micro</h1>
-						</Col>
-					</Row>
-				</Container>
+			<div className="wrapper">
+				<h1>News</h1>
+			</div>
+			<Container className="news">
+				<Row className="justify-content-center">
+					<Col>
+						<ul>
+							<li onClick={() => setModalShow(true)}>
+								Purchase Order Policies
+							</li>
+							<li>
+								New Year's Message
+							</li>
+						</ul>
+					</Col>
+				</Row>
+			</Container>
 			<Footer />
+
+		{/* Modal for Policies */}
+			<POPolicies
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
 		</>
 	);
 }
