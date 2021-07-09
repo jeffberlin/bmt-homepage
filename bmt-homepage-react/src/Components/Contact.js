3edf64b228712605a775928adecadda4923607fa
jeffberlin <jeff@jbrally.com>
2021-07-09 16:02:13 -0400

    Building contact react page

--- bmt-homepage-react/src/Components/Contact.js
+++ bmt-homepage-react/src/Components/Contact.js
@@ -9,6 +9,9 @@ function Contact() {
 			</div>
 			<Container className="contact">
 				<Row>
+					<p>With BMT Micro, you can always get in touch with a real person who can help and assist you with any problems you may have. Unlike many of our competitors, </p>
+				</Row>
+				<Row>
 					<Col xl={12} lg={12} md={12} sm={12} style={{ marginBottom: '2rem' }}>
 						<CardDeck>
 							<Card>
@@ -23,8 +26,8 @@ function Contact() {
 								<Card.Img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22321%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20321%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17a68ae5800%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2C%26quot%3BLiberation%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17a68ae5800%22%3E%3Crect%20width%3D%22321%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22120.0234375%22%20y%3D%2287.5375%22%3E321x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
 								<Card.Body>
 									<Card.Title>Fax</Card.Title>
-									<Card.Text>24-hour FAX (toll-free in USA and Canada):&nbsp;1-800-346-1672</Card.Text>
-									<Card.Text>24-hour FAX:&nbsp;1-910-792-1889</Card.Text>
+									<Card.Text>24-hour FAX (toll-free in USA and Canada):&nbsp;<span style={{ color: '#efa900' }}>1-800-346-1672</span></Card.Text>
+									<Card.Text>24-hour FAX:&nbsp;<span style={{ color: '#efa900' }}>1-910-792-1889</span></Card.Text>
 									<Card.Text style={{ fontSize: '.9rem' }}>This is an alternate way to send in orders, questions, or comments. <strong>Please note:</strong> Faxed orders will be processed the following business day.</Card.Text>
 								</Card.Body>
 							</Card>
@@ -45,7 +48,7 @@ function Contact() {
 								<Card.Body>
 									<Card.Title>Mail</Card.Title>
 									<Card.Text>Address to PO Box:</Card.Text>
-									<Card.Text>BMT Micro P.O. Box 15016<br/>Wilmington, NC 28408 USA</Card.Text>
+									<Card.Text style={{ color: '#efa900' }}>BMT Micro P.O. Box 15016<br/>Wilmington, NC 28408 USA</Card.Text>
 								</Card.Body>
 							</Card>
 						</CardDeck>
