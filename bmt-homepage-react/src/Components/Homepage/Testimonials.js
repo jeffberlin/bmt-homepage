f8d2aae7189ed0302136937719664050a671ea1c
Jeff Berlin <jeff@jbrally.com>
2021-08-26 09:35:25 -0400

    Optimizing & building
    Adding array for testimonials. Adding new pricing background

--- bmt-homepage-react/src/Components/Homepage/Testimonials.js
+++ bmt-homepage-react/src/Components/Homepage/Testimonials.js
@@ -2,6 +2,37 @@ import React from 'react';
 import { Container, Row, Col, Carousel } from 'react-bootstrap';
 
 function Testimonials() {
+	const testimonials = [
+		{
+			"text": "It’s extremely low-friction. Sign up for an account, add products, add serial numbers, add payment links to your website and you’re done. Checks come once a month like clockwork. Much easier than Paypal and reduces the headaches.",
+			"name": "Kevin W."
+		},
+		{
+			"text": "BMT Micro on the other hand are a joy to work with and they treat customers like royalty... quick support and they are happy to go the extra mile. Highly recommended.",
+			"name": "Frank R."
+		},
+		{
+			"text": "You have exemplified superior customer service.",
+			"name": "Brian A."
+		},
+		{
+			"text": "BMT Micro, and you’ll never look back. Outstanding support, always above and beyond the call of duty. Been with them for over 10 years and I ain’t changing for anything. Some things simply have no price... They are extremely professional and, above all, very friendly.",
+			"name": "Jorge C."
+		},
+		{
+			"text": "As a consumer, I'd much rather see indie devs using BMT Micro – simply because it gives me more payment options, and crucially – payment options that do not involved credit card (while still having a credit card option for those who want it).",
+			"name": "Alexander P."
+		},
+		{
+			"text": "I am so impressed with BMT's outstanding customer service!",
+			"name": "Lenora"
+		},
+		{
+			"text": "This is the best customer service I’ve ever received… To respond within a day, too, is extraordinary. I can’t thank you enough!",
+			"name": "Chris S."
+		}
+	]
+
 	return (
 		<div className="testimonials">
 			<Container>
@@ -11,66 +42,16 @@ function Testimonials() {
 							controls={false}
 							indicators={false}
 						>
-							<Carousel.Item interval={6500}>
-								<p style={{ marginBottom: '.5rem' }}>
-									<em>
-										"It’s extremely low-friction. Sign up for an account, add products, add serial numbers, add payment links to your website and you’re done. Checks come once a month like clockwork. Much easier than Paypal and reduces the headaches."
-									</em>
-								</p>
-								<p style={{ textAlign: 'center', marginBottom: '0' }}>
-									<strong>
-										-&nbsp;Kevin W.
-									</strong>
-								</p>
-							</Carousel.Item>
-							<Carousel.Item interval={6500}>
-								<p style={{ marginBottom: '.5rem' }}>
-									<em>
-										"BMT Micro on the other hand are a joy to work with and they treat customers like royalty... quick support and they are happy to go the extra mile. Highly recommended."
-									</em>
-								</p>
-								<p style={{ textAlign: 'center', marginBottom: '0' }}>
-									<strong>
-										-&nbsp;Frank R.
-									</strong>
-								</p>
-							</Carousel.Item>
-							<Carousel.Item interval={6500}>
-								<p style={{ marginBottom: '.5rem' }}>
-									<em>
-										"BMT Micro, and you’ll never look back. Outstanding support, always above and beyond the call of duty. Been with them for over 10 years and I ain’t changing for anything. Some things simply have no price... They are extremely professional and, above all, very friendly."
-									</em>
-								</p>
-								<p style={{ textAlign: 'center', marginBottom: '0' }}>
-									<strong>
-										-&nbsp;Jorge C.
-									</strong>
-								</p>
-							</Carousel.Item>
-							<Carousel.Item interval={6500}>
-								<p style={{ marginBottom: '.5rem' }}>
-									<em>
-										"As a consumer, I'd much rather see indie devs using BMT Micro – simply because it gives me more payment options, and crucially – payment options that do not involved credit card (while still having a credit card option for those who want it)."
-									</em>
-								</p>
-								<p style={{ textAlign: 'center', marginBottom: '0' }}>
-									<strong>
-										-&nbsp;Alexander P.
-									</strong>
-								</p>
-							</Carousel.Item>
-							<Carousel.Item interval={3000}>
-								<p style={{ marginBottom: '.5rem', textAlign: 'center' }}>
-									<em>
-										"I am so impressed with BMT's outstanding customer service!"
-									</em>
-								</p>
-								<p style={{ textAlign: 'center', marginBottom: '0' }}>
-									<strong>
-										-&nbsp;Lenora
-									</strong>
-								</p>
-							</Carousel.Item>
+							{testimonials.map(testimonial => (
+								<Carousel.Item interval={6500}>
+									<p style={{ marginBottom: '.5rem' }}>
+										<em>"{testimonial.text}"</em>
+									</p>
+									<p style={{ textAlign: 'center', marginBottom: '0' }}>
+										<strong>-&nbsp;{testimonial.name}</strong>
+									</p>
+								</Carousel.Item>
+							))}
 						</Carousel>
 					</Col>
 				</Row>
--- bmt-homepage-react/src/Components/Pricing/Pricing.js
+++ bmt-homepage-react/src/Components/Pricing/Pricing.js
@@ -9,7 +9,7 @@ function Pricing() {
     <div className="body-content">
       <div className="pricing-background"></div>
       <Container className="pricing">
-        <h5 className="text-center" style={{ letterSpacing: '.2px', marginBottom: '2rem', color: '#000000' }}>At BMT Micro, we believe in doing what is best for our vendors and can custom tailor a plan that works best for you!</h5>
+        <h5 className="text-center" style={{ letterSpacing: '.2px', marginBottom: '2rem', color: '#ffffff' }}>At BMT Micro, we believe in doing what is best for our vendors and can custom tailor a plan that works best for you!</h5>
         <Row>
           <BasicTier />
           <AdvancedTier />
