3aad5224632ebf1832eb338b8285c171a3f25834
jeffberlin <jeff@jbrally.com>
2021-10-25 11:02:15 -0400

    Working on FAQ.js

--- bmt-homepage-react/src/Components/FAQ.js
+++ bmt-homepage-react/src/Components/FAQ.js
@@ -5,14 +5,13 @@ import faqs from './FAQArray';
 function FAQ() {
 	return (
 		<div className="body-content">
-			{/*<div className="wrapper">
-				<h1>FAQ's</h1>
-			</div>*/}
 			<Container className="faq">
 				<Row className="justify-content-center" style={{ marginBottom: '3rem' }}>
-					<Col xl={4} lg={4} md={4} sm={4}>
+					<CardDeck>
+					{/* <Col xl={4} lg={4} md={4} sm={4}> */}
+
 						<a href="https://help.bmtmicro.com/vendors/" target="_blank" rel="noreferrer">
-							<Card>
+							<Card style={{ width: 250 }}>
 								<Card.Body className="text-center">
 									<Card.Title>
 										<i className="fas fa-laptop-code"></i>
@@ -21,15 +20,15 @@ function FAQ() {
 										Vendor Support
 									</Card.Title>
 									<Card.Text>
-										Help pages for BMT Micro vendors
+										Help pages for vendors
 									</Card.Text>
 								</Card.Body>
 							</Card>
 						</a>
-					</Col>
-					<Col xl={4} lg={4} md={4} sm={4}>
+					{/* </Col>
+					<Col xl={4} lg={4} md={4} sm={4}> */}
 						<a href="https://help.bmtmicro.com/customers/" target="_blank" rel="noreferrer">
-							<Card>
+							<Card style={{ width: 250 }}>
 								<Card.Body className="text-center">
 									<Card.Title>
 										<i className="fas fa-shopping-cart"></i>
@@ -43,7 +42,8 @@ function FAQ() {
 								</Card.Body>
 							</Card>
 						</a>
-					</Col>
+					{/* </Col> */}
+				</CardDeck>
 				</Row>
 				<Row className="justify-content-center">
 					{faqs.map((faq, index) => (
