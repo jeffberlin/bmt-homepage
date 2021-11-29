09fe632a5400ee821115c2a90f7c3fc6aa5d3a4d
jeffberlin <jeff@jbrally.com>
2021-11-29 15:51:26 -0500

    Updating NPM

--- bmt-homepage-react/src/Components/About.js
+++ bmt-homepage-react/src/Components/About.js
@@ -14,7 +14,7 @@ function About() {
 					    	<source src="movie.ogg" type="video/ogg" />
 					    	Your browser does not support the video tag.
 					    </video> */}
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/vF-MgzLXfmo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/vF-MgzLXfmo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 					  </ResponsiveEmbed>
 					</Col>
 				</Row>
