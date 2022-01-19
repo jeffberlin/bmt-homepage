b86f2145202c4c8a8587ef7937b2e53f2c4c1327
Jeff Berlin <jeff@jbrally.com>
2022-01-19 17:09:24 -0500

    Removing old README

--- README.md
+++
@@ -1,4 +0,0 @@
-# bmt-homepage
-Potential new homepage for BMT Micro
-
-https://jeffberlin.github.io/bmt-homepage/
--- docs/js/main.js
+++
@@ -1,4 +0,0 @@
-// opens dropdown menu on hover
-$('ul.navbar-nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').toggleClass('show');
-});
