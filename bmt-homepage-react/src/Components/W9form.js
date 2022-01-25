a052c817403267cfb0275f31d58bd1242c03c4fc
Jeff Berlin <jeff@jbrally.com>
2022-01-25 15:45:15 -0500

    Updating W9 form

--- bmt-homepage-react/src/Components/W9form.js
+++ bmt-homepage-react/src/Components/W9form.js
@@ -11,8 +11,8 @@ function W9form() {
         <Row>
           <Col>
             <p>If you need to download the W-9 Form, please click the button below.</p>
-            <a href="https://www.bmtmicro.com/images/W9_2019.pdf" download>
-              <i className="fas fa-file-download"></i>&nbsp;&nbsp;W9_2019.pdf
+            <a href="https://www.bmtmicro.com/images/W9_2022.pdf" download="BMT_Micro_W9_Form_2022.pdf">
+              <i className="fas fa-file-download"></i>&nbsp;&nbsp;W9_2022.pdf
             </a>
           </Col>
         </Row>
