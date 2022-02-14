5d95a7d8934cc3c6053f88801e773e6308623992
jeffberlin <jeff@jbrally.com>
2022-02-14 16:01:40 -0500

    Building product-overview page

--- bmt-homepage-react/src/Components/ProductOverview.js
+++ bmt-homepage-react/src/Components/ProductOverview.js
@@ -5,9 +5,24 @@ function ProductOverview() {
   return (
     <div className="body-content">
       <Container fluid className="product-overview">
+        <Row className="justify-content-center">
+          <Col xl={6} lg={6}>
+            <h1>Whatever your needs,</h1>
+            <h1>BMT Micro has a solution for you</h1>
+            {/* <div className="line"></div> */}
+          </Col>
+        </Row>
+        <Row>
+          <Col xl={{span: 7, offset: 2}} lg={{span: 7, offset: 2}} md={{span: 7, offset: 2}}>
+            <div className="line"></div>
+          </Col>
+        </Row>
         <Row>
           <Col>
-            <h1>Increase your sales</h1>
+            <p>WordPress Plugin</p>
+            <p>Custom Cart</p>
+            <p>Dashboard, upload products, track sales, emails,</p>
+            <p>Pop-up cart</p>
           </Col>
         </Row>
       </Container>
