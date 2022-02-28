0ac13707d9c7344132023806aa71e23801e5b545
Jeff Berlin <jeff@jbrally.com>
2022-02-28 10:02:08 -0500

    Building product overview

--- bmt-homepage-react/src/Components/ProductOverview.js
+++ bmt-homepage-react/src/Components/ProductOverview.js
@@ -4,24 +4,53 @@ import { Container, Row, Col } from 'react-bootstrap';
 function ProductOverview() {
   return (
     <div className="body-content">
-      <Container fluid className="product-overview">
+      <Container className="product-overview">
         <Row className="justify-content-center">
-          <Col xl={6} lg={6}>
+          <Col xl={10} lg={10}>
             <h1>Whatever your needs,</h1>
             <h1>BMT Micro has a solution for you</h1>
           </Col>
         </Row>
         <Row>
-          <Col xl={{span: 7, offset: 2}} lg={{span: 7, offset: 2}} md={{span: 7, offset: 2}}>
+          <Col xl={12} lg={12} md={12} sm={12}>
             <div className="line"></div>
           </Col>
         </Row>
-        <Row>
+        <Row className="justify-content-center" style={{ marginBottom: '5rem' }}>
           <Col>
-            <p>WordPress Plugin</p>
-            <p>Custom Cart</p>
-            <p>Dashboard, upload products, track sales, emails,</p>
-            <p>Pop-up cart</p>
+            <p>[screenshot]</p>
+          </Col>
+          <Col xl={6} lg={6} md={12} sm={12}>
+            <h2>Customized Shopping Cart</h2>
+            <p>Select options that you would like to have displayed in your cart, blend your cart into your current website design, and much more!</p>
+          </Col>
+        </Row>
+        <Row className="justify-content-center" style={{ marginBottom: '5rem' }}>
+          <Col xl={6} lg={6} md={12} sm={12}>
+            <h2>Pop-up Cart</h2>
+            <p>Easy to implement and </p>
+          </Col>
+          <Col>
+            <p>[screenshot]</p>
+          </Col>
+        </Row>
+        <Row className="justify-content-center" style={{ marginBottom: '5rem' }}>
+          <Col>
+            <p>[screenshot]</p>
+          </Col>
+          <Col xl={6} lg={6} md={12} sm={12}>
+            <h2>WordPress Plugin</h2>
+            <p>If you have a WordPress website we have you covered with our BMT Micro Shopping Cart Plugin! The plugin utilizes our "pop-up" cart and integrates into your WordPress theme.</p>
+            <a href="https://help.bmtmicro.com/plugin/" target="_blank" rel="noreferrer" className="plugin-btn"><span>Learn More</span>&nbsp;&nbsp;<i className="fas fa-arrow-right"></i></a>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xl={6} lg={6} md={12} sm={12}>
+            <h2>Supplier Portal</h2>
+            <p>With our supplier portal, you can manage your products, track sales, edit your account, and much more. </p>
+          </Col>
+          <Col>
+            <p>[screenshot]</p>
           </Col>
         </Row>
       </Container>
