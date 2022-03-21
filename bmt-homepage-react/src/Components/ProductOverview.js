3e44e17de6d42f29c19f854d6356706234b59125
Jeff Berlin <jeff@jbrally.com>
2022-03-21 09:32:42 -0400

    Building product overview page

--- bmt-homepage-react/src/Components/ProductOverview.js
+++ bmt-homepage-react/src/Components/ProductOverview.js
@@ -16,29 +16,35 @@ function ProductOverview() {
             <div className="line"></div>
           </Col>
         </Row>
-        <Row className="justify-content-center" style={{ marginBottom: '7rem' }}>
-          <Col>
-            <p>[screenshot]</p>
+        <Row className="justify-content-center align-items-center" style={{ marginBottom: '7rem' }}>
+          <Col className="text-center">
+            <Image
+              src="../images/bmt-popup-cart-overview-min.png"
+              width={400}
+            />
           </Col>
+          <Col xl={6} lg={6} md={12} sm={12}>
+            <h2>Pop-up Cart</h2>
+            <p>If you prefer to implement your own options</p>
+          </Col>
+        </Row>
+        <Row className="justify-content-center align-items-center" style={{ marginBottom: '7rem' }}>
           <Col xl={6} lg={6} md={12} sm={12}>
             <h2>Customized Shopping Cart</h2>
             <p>Select options that you would like to have displayed in your cart, blend your cart into your current website design, and much more!</p>
           </Col>
-        </Row>
-        <Row className="justify-content-center" style={{ marginBottom: '7rem' }}>
-          <Col xl={6} lg={6} md={12} sm={12}>
-            <h2>Pop-up Cart</h2>
-            <p>Easy to implement and </p>
-          </Col>
-          <Col>
-            <p>[screenshot]</p>
+          <Col className="text-center">
+            <Image
+              src="../images/custom-cart-template.png"
+              width={400}
+            />
           </Col>
         </Row>
         <Row className="justify-content-center align-items-center" style={{ marginBottom: '7rem' }}>
-          <Col>
+          <Col className="text-center">
             <Image
               src="../images/bmt-micro-wordpress-plugin-image.png"
-              fluid
+              width={400}
             />
           </Col>
           <Col xl={6} lg={6} md={12} sm={12}>
@@ -47,12 +53,12 @@ function ProductOverview() {
             <a href="https://help.bmtmicro.com/plugin/" target="_blank" rel="noreferrer" className="plugin-btn"><span>Learn More</span>&nbsp;&nbsp;<i className="fas fa-arrow-right"></i></a>
           </Col>
         </Row>
-        <Row className="justify-content-center">
+        <Row className="justify-content-center align-items-center">
           <Col xl={6} lg={6} md={12} sm={12}>
             <h2>Supplier Portal</h2>
             <p>With our supplier portal, you can manage your products, track detailed sales information, edit your account, add users, and much more. </p>
           </Col>
-          <Col>
+          <Col className="text-center">
             <p>[screenshot]</p>
           </Col>
         </Row>
