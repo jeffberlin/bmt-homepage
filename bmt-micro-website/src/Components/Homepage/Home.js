b92fd9a053211ba97afab0af9480ae1ede34cc4c
jeffberlin <jeff@jbrally.com>
2022-04-25 16:00:26 -0400

    Building new react version

---
+++ bmt-micro-website/src/Components/Homepage/Home.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+function Home() {
+  useEffect(() => {
+    document.title = "BMT Micro - Global Digital Commerce Platform"
+  })
+  return (
+    
+  );
+}
+
+export default Home;
